8ff55167fc38f8082dbd713a995e60c0dc6d01b8
Julian Stecklina <js@alien8.de>
2023-02-07 17:17:00 +0100

    nix: use flake-parts

--- flake.nix
+++ flake.nix
@@ -3,41 +3,52 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs }: let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      jekyll_env = pkgs.bundlerEnv rec {
-        name = "jekyll_env";
-        gemfile = ./Gemfile;
-        lockfile = ./Gemfile.lock;
-        gemset = ./gemset.nix;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+
+        # Not actively tested, but should work:
+        # "aarch64-linux"
+      ];
+
+      perSystem = { config, system, pkgs, ... }: {
+        packages = {
+          jekyll_env = pkgs.bundlerEnv {
+            name = "jekyll_env";
+            gemfile = ./Gemfile;
+            lockfile = ./Gemfile.lock;
+            gemset = ./gemset.nix;
+          };
+
+          blog = pkgs.stdenv.mkDerivation {
+            name = "x86-lol";
+
+            src = ./.;
+            nativeBuildInputs = [ config.packages.jekyll_env ];
+
+            dontConfigure = true;
+
+            buildPhase = "jekyll build";
+            installPhase = ''
+              mkdir -p $out
+              cp -r _site/* $out/
+            '';
+          };
+
+          default = config.packages.blog;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ config.packages.jekyll_env ];
+
+          shellHook = ''
+            exec jekyll serve --watch --drafts
+          '';
+        };
       };
-  in {
-    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-      name = "x86-lol";
-
-      src = ./.;
-      nativeBuildInputs = [ jekyll_env ];
-
-      dontConfigure = true;
-
-      buildPhase = ''
-        jekyll build
-      '';
-
-      installPhase = ''
-        mkdir -p $out
-        cp -r _site/* $out/
-      '';
     };
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [ jekyll_env ];
-
-      shellHook = ''
-        exec jekyll serve --watch --drafts
-      '';
-    };
-  };
 }
